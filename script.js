adc404eb77260071e4caec86158ab4f4eec47c93
bolotskih <gafer.bae@icloud.com>
2019-07-22 10:06:23 +0400

    jsadd

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-console.log("Hello world");
+var o="Hellу Evgeny",l=prompt("Сколько было яблок у Мазая?",2),e=l*l;console.log(o),console.log("string"),console.log(123),console.log("number"),console.log(!1),console.log(typeof!1),console.log("undefined"),alert(l),console.log(typeof l),alert(e),console.log(typeof e);
